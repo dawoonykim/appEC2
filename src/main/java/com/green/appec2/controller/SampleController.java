1e899856b13acb8fb57d0bec450a1372b4325dfc
dawoony_kim <dawoonk111@gmail.com>
2024-05-08 23:49:19 +0900

    자바 버전 맞추기

--- src/main/java/com/green/appec2/controller/SampleController.java
+++ src/main/java/com/green/appec2/controller/SampleController.java
@@ -3,6 +3,7 @@ package com.green.appec2.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//24.05.08
 @RestController
 @RequestMapping("/api/sample")
 public class SampleController {
