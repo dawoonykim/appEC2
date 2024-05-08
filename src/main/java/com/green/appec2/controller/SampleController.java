f99d032cac9e77911e0ee7c5c5b535e7082690e5
dawoony_kim <dawoonk111@gmail.com>
2024-05-08 23:28:23 +0900

    Deploy_Test

---
+++ src/main/java/com/green/appec2/controller/SampleController.java
@@ -0,0 +1,14 @@
+package com.green.appec2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sample")
+public class SampleController {
+
+    @RequestMapping("/getArr")
+    public String[] gettArr() {
+        return new String[]{"A,B,C"};
+    }
+}
